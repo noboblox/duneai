aeef3942af4927d87e6fbf9fb1872a2bfacd5566
blox <blox@posteo.de>
2022-12-01 19:32:30 +0100

    move main into example.cc

---
+++ duneai/example.cc
@@ -0,0 +1,33 @@
+#include "gamelogic.h"
+
+int main()
+{
+	GameLogic game(Faction::any(), 4004030159);
+	game.post(std::make_unique<ActionPrediction>(Faction::beneGesserit(), Faction::atreides(), 5));
+
+	game.post(std::make_unique<ActionHarkonnenRedraw> (Faction::harkonnen(),    true));
+	game.post(std::make_unique<ActionTraitorSelection>(Faction::emperor(),      Leader::ID_Irulan));
+	game.post(std::make_unique<ActionTraitorSelection>(Faction::spacingGuild(), Leader::ID_Burseg));
+	game.post(std::make_unique<ActionTraitorSelection>(Faction::fremen(),       Leader::ID_Alia));
+	game.post(std::make_unique<ActionTraitorSelection>(Faction::atreides(),     Leader::ID_Feyd));
+	game.post(std::make_unique<ActionTraitorSelection>(Faction::beneGesserit(), Leader::ID_Duncan));
+	game.post(std::make_unique<ActionTraitorSelection>(Faction::tleilaxu(),     Leader::ID_Wanna));
+
+	// fail
+//	game.post(std::make_unique<ActionFremenPlacement>(Faction::fremen(), std::vector<Placement>{Placement{Arrakis::FalseWallSouth_5,105, 3},
+//			                                                                                    Placement{Arrakis::FalseWallWest_18, 1, 6}}));
+
+	// fail
+//	game.post(std::make_unique<ActionFremenPlacement>(Faction::fremen(), std::vector<Placement>{Placement{Arrakis::FalseWallSouth_5, 1, 2},
+//			                                                                                    Placement{Arrakis::FalseWallWest_18, 1, 6}}));
+	// fail
+//	game.post(std::make_unique<ActionFremenPlacement>(Faction::fremen(), std::vector<Placement>{Placement{Arrakis::FalseWallSouth_5,105, 3},
+//			                                                                                    Placement{Arrakis::TheGreatFlat, 1, 6}}));
+
+	// success
+	game.post(std::make_unique<ActionFremenPlacement>(Faction::fremen(), std::vector<Placement>{Placement{Arrakis::FalseWallSouth_5, 5, 3},
+			                                                                                    Placement{Arrakis::FalseWallWest_18, 1, 1}}));
+
+	game.tick();
+	return 0;
+}
