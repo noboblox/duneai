ada5294a97d4321a44601f996696b8ae20003cbf
blox <blox@posteo.de>
2023-11-24 21:18:10 +0100

    move implementation of MessageThreadd to source file

---
+++ duneai/messagethread.cc
@@ -0,0 +1,53 @@
+#include "messagethread.h"
+#include <thread>
+
+MessageThread::MessageThread()
+{
+	auto thread = std::thread([this]() { this->messageLoop(); });
+	thread.detach();
+}
+
+
+MessageThread::~MessageThread() noexcept
+{
+	shutdown();
+}
+
+void MessageThread::post(std::unique_ptr<Message>&& msg) {
+	if (msg == nullptr)
+		return;
+
+	std::lock_guard<std::mutex> l(mQueueMutex);
+	mPending.push_back(std::move(msg));
+}
+
+void MessageThread::messageLoop()
+{
+	while (!mNeedStop)
+	{
+		auto p_msg = popMessage();
+
+		if (p_msg)
+			executeMessage(std::move(p_msg));
+		else
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+}
+
+void MessageThread::shutdown() noexcept
+{
+	mNeedStop = true;
+}
+
+std::unique_ptr<Message> MessageThread::popMessage()
+{
+	std::lock_guard<std::mutex> l(mQueueMutex);
+
+	if (mPending.empty())
+		return nullptr;
+
+	auto p_msg = std::move(mPending.front());
+	mPending.erase(mPending.begin());
+
+	return std::move(p_msg);
+}
--- duneai/messagethread.h
+++ duneai/messagethread.h
@@ -4,7 +4,6 @@
 #include <atomic>
 #include <memory>
 #include <mutex>
-#include <thread>
 #include <vector>
 
 #include "deletedefaults.h"
@@ -14,61 +13,18 @@ class MessageThread
 {
 public:
 	MACRO_DELETE_ALL_DEFAULTS(MessageThread)
-		
-	explicit MessageThread()
-	{
-		auto thread = std::thread([this]() { this->messageLoop(); });
-		thread.detach();
-	}
 
+	explicit MessageThread();
+	virtual ~MessageThread() noexcept;
 
-	virtual ~MessageThread() noexcept
-	{
-		shutdown();
-	}
-
-	void post(std::unique_ptr<Message>&& msg) {
-		if (msg == nullptr)
-			return;
-
-		std::lock_guard<std::mutex> l(mQueueMutex);
-		mPending.push_back(std::move(msg));
-	}
+	void post(std::unique_ptr<Message>&& msg);
 
 private:
 	virtual void executeMessage(std::unique_ptr<Message>&& msg) = 0;
 
-	void messageLoop()
-	{
-		while (!mNeedStop)
-		{
-			auto p_msg = popMessage();
-
-			if (p_msg)
-				executeMessage(std::move(p_msg));
-			else
-				std::this_thread::sleep_for(std::chrono::milliseconds(10));
-		}
-	}
-
-	void shutdown() noexcept
-	{
-		mNeedStop = true;
-	}
-
-private:
-	std::unique_ptr<Message> popMessage()
-	{
-		std::lock_guard<std::mutex> l(mQueueMutex);
-
-		if (mPending.empty())
-			return nullptr;
-
-		auto p_msg = std::move(mPending.front());
-		mPending.erase(mPending.begin());
-
-		return std::move(p_msg);
-	}
+	void messageLoop();
+	void shutdown() noexcept;
+	std::unique_ptr<Message> popMessage();
 
 private:
 	std::atomic<bool> mNeedStop = false;
